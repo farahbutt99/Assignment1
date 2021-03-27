f2a2d14aea7db195c750f8746aa35fb8b82fb4f1
farahbutt99 <mcsf19a021@pucit.edu.pk>
2021-03-27 20:25:32 +0500

    Duas list added

--- Assignment_IslamicDuas/app/src/main/java/com/example/assignment1_islamicduas/HomeActivity.java
+++ Assignment_IslamicDuas/app/src/main/java/com/example/assignment1_islamicduas/HomeActivity.java
@@ -3,6 +3,10 @@ package com.example.assignment1_islamicduas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ListView listView = findViewById(R.id.ListViewDuas);
+        ArrayList<String> arrayListDuas= new ArrayList<String>();
+        arrayListDuas.add("1۔ علم میں اضافے کی دعا");
+        arrayListDuas.add("2۔سفر کی دعا");
+        arrayListDuas.add("3۔ اذان کے بعد کی دعا");
+        arrayListDuas.add("4۔ غصے کی دعا");
+        arrayListDuas.add("5۔جسم میں درد کی دعا");
+        arrayListDuas.add("6");
+        arrayListDuas.add("7");
+        arrayListDuas.add("8");
+        arrayListDuas.add("9");
+        arrayListDuas.add("10");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1 ,arrayListDuas);
+       listView.setAdapter(arrayAdapter);
     }
 }
--- Assignment_IslamicDuas/app/src/main/java/com/example/assignment1_islamicduas/MainActivity.java
+++ Assignment_IslamicDuas/app/src/main/java/com/example/assignment1_islamicduas/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -19,13 +20,16 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT= 4000;
+    private static int SPLASH_TIME_OUT= 8000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        MediaPlayer mp = MediaPlayer.create(this,R.raw.bismillah);
+        TextView textView= findViewById(R.id.textView2);
+        textView.animate().alpha(0).scaleXBy(0.5f).scaleYBy(0.5f).setDuration(8000);
+        mp.start();
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run(){
