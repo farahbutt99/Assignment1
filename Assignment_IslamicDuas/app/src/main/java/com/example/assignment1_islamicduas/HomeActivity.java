c7bdad07ba36d88e24e26d1c18f844149cefbbf4
farahbutt99 <mcsf19a021@pucit.edu.pk>
2021-03-27 15:01:40 +0500

    first commit

---
+++ Assignment_IslamicDuas/app/src/main/java/com/example/assignment1_islamicduas/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.assignment1_islamicduas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ Assignment_IslamicDuas/app/src/main/java/com/example/assignment1_islamicduas/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.assignment1_islamicduas;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT= 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent homeIntent= new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+
+
+}
