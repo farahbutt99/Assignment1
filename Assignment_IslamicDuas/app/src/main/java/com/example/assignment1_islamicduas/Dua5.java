c54cee05d6adf28531d62ee1672d0c41cf9b73f4
farahbutt99 <mcsf19a021@pucit.edu.pk>
2021-04-02 15:21:24 +0500

    adding duas a

---
+++ Assignment_IslamicDuas/app/src/main/java/com/example/assignment1_islamicduas/Dua5.java
@@ -0,0 +1,14 @@
+package com.example.assignment1_islamicduas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dua5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dua5);
+    }
+}
---
+++ Assignment_IslamicDuas/app/src/main/java/com/example/assignment1_islamicduas/Dua6.java
@@ -0,0 +1,14 @@
+package com.example.assignment1_islamicduas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dua6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dua6);
+    }
+}
